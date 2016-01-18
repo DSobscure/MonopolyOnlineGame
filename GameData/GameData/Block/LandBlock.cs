1a4d8387360daea188b535b722ae31273e18c409
DSobscure <snbruce13@yahoo.com.tw>
2016-01-18 21:54:10 +0800

    test function buy land selection

--- GameData/GameData/Block/LandBlock.cs
+++ GameData/GameData/Block/LandBlock.cs
@@ -12,40 +12,6 @@ namespace MonopolyGame
         public LandBlock(Map map, Land land) : base(map)
         {
             this.land = land;
-            OnTokenPlaceInto += BuyLandSelectionEventTask;
-            OnTokenPlaceInto += UpgradeLandSelectionEventTask;
-            OnTokenPlaceInto += PayForTollEventTask;
-        }
-
-        private void BuyLandSelectionEventTask(Block block, Token token)
-        {
-            LandBlock landBlock = block as LandBlock;
-            if (landBlock.land.owner == null)
-            {
-                if (landBlock.OnBuyLandSelection != null)
-                    landBlock.OnBuyLandSelection(token.owner, land);
-                //map.game.response = ResponseType.BuyLandSelection;
-            }
-        }
-        private void UpgradeLandSelectionEventTask(Block block, Token token)
-        {
-            LandBlock landBlock = block as LandBlock;
-            if (landBlock.land.owner == token.owner)
-            {
-                if (landBlock.land.isUpgradable && (landBlock.OnUpgradeLandSelection != null))
-                    landBlock.OnUpgradeLandSelection(token.owner, land);
-                //map.game.response = ResponseType.UpgradeLandSelection;
-            }
-        }
-        private void PayForTollEventTask(Block block, Token token)
-        {
-            LandBlock landBlock = block as LandBlock;
-            if (landBlock.land.owner != null && landBlock.land.owner != token.owner)
-            {
-                if (landBlock.OnPayForToll != null)
-                    landBlock.OnPayForToll(token.owner, land.owner, land.toll);
-                //map.game.response = ResponseType.PayForTollMessage;
-            }
         }
     }
 }
